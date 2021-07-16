3ffb0d99419a9ef7d3accc09f862645b30faf305
Shawn Cochran <shawn.cochran31@gmail.com>
2021-07-16 17:27:51 -0600

    feature-arrays, added arrays

---
+++ Develop/script.js
@@ -0,0 +1,37 @@
+// Assignment code here
+var special = [
+  '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?'
+]
+
+var number = [
+  '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
+]
+
+var lower = [
+ ' a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+
+var upper = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
+
+
+
+
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
